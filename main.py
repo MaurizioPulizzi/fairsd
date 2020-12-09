7c84f5078e6ee683619dac9f635d8653d604686a
MaurizioPulizzi <m.pulizzi@student.tue.nl>
2020-12-09 21:04:28 +0100

    Added comments.

--- main.py
+++ main.py
@@ -2,30 +2,13 @@ from sklearn.datasets import fetch_openml
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-from fairlearn.metrics import MetricFrame
-import numpy as np
+#from fairlearn.metrics import MetricFrame
+#import numpy as np
 '''author: Maurizio'''
 
 import DiscriminatedSubgroupsDiscovery as dsd
 
-class EqualOpportunity(dsd.QualityFunction):
-    def evaluate(self, description, task):
-        data = task.data
-        target_attr = task.target.target_attribute
-        target_val = task.target.target_value
-        pred_attr = task.target.predicted_target_attr
 
-        p_subset = data[description.to_boolean_array(data) & (data[target_attr] == target_val)]
-        p_sg = p_subset.shape[0]
-        tp_sg = p_subset[(p_subset[pred_attr] == target_val)].shape[0]
-
-        p_complement = data[description.complement_to_boolean_array(data) & (data[target_attr] == target_val)]
-        p_c = p_complement.shape[0]
-        tp_c = p_complement[(p_complement[pred_attr] == target_val)].shape[0]
-
-        if p_sg == 0: return 0
-        if p_c == 0: return 0
-        return (tp_c / p_c) - (tp_sg/p_sg)
 
 #Import dataset, training the classifier, producing y_pred
 d = fetch_openml(data_id=1590, as_frame=True)
@@ -43,7 +26,7 @@ dataset['y_true'] = y_true
 dataset['y_pred'] = y_pred
 #dataset=dataset.head(10)
 
-target= dsd.BinaryTarget('y_true',dataset, 'y_pred', target_value=True)
+target= dsd.BinaryTarget(target_attribute='y_true',dataset=dataset, predicted_target_attr='y_pred', target_value=True)
 
 search_space=dsd.SearchSpace(dataset, ignore=['y_true', 'y_pred', 'fnlwgt', 'education'])
 
@@ -54,7 +37,7 @@ task=dsd.SubgroupDiscoveryTask(
         target,
         search_space,
         discretizer =discretizer,
-        qf=EqualOpportunity(),
+        qf=dsd.EqualOpportunity(),
         min_support=250,
         result_set_size=10)
 
